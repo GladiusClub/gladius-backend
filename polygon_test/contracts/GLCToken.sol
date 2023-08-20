be35bb54337f08a0cd4c4f9c58407a8c3ebc514b
Alexey Sirotkin <alexey.sirotkin@telia.ee>
2023-08-20 19:28:23 +0300

    emit an event inside smart contract

--- polygon_test/contracts/GLCToken.sol
+++ polygon_test/contracts/GLCToken.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.0;
 
 import "node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
 contract GLCToken is ERC20 {
     address private _owner;
+
+    // Event to record transactions
+    event TransactionRecord(address indexed from, address indexed to, uint256 amount);
 
     constructor(uint256 initialSupply) ERC20("GLC Token", "GLC") {
         _mint(msg.sender, initialSupply);
@@ -13,7 +15,6 @@ contract GLCToken is ERC20 {
     }
 
     function approve(address spender, uint256 amount) public virtual override returns (bool) {
-        require(msg.sender == _owner, "Only the contract owner can approve");
         _approve(msg.sender, spender, amount);
         return true;
     } 
@@ -21,10 +22,17 @@ contract GLCToken is ERC20 {
     function mint(address to, uint256 amount) public {
         require(msg.sender == _owner, "Only the contract owner can mint tokens");
         _mint(to, amount);
+
+        // Emit the TransactionRecord event
+        emit TransactionRecord(msg.sender, to, amount);
     }
 
     function transfer(address to, uint256 amount) public override returns (bool) {
         _transfer(msg.sender, to, amount);
+
+        // Emit the TransactionRecord event
+        emit TransactionRecord(msg.sender, to, amount);
+
         return true;
     }
 }
