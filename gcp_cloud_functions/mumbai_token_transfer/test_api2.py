90b44ffada243b91f6bdb3962b503f162bf25cef
Alexey Sirotkin <alexey.sirotkin@telia.ee>
2023-08-21 20:28:15 +0300

    add validation rules

---
+++ gcp_cloud_functions/mumbai_token_transfer/test_api2.py
@@ -0,0 +1,45 @@
+import requests
+import time
+# Replace with the URL of your deployed Google Cloud Function
+function_url = "https://us-central1-wallet-login-45c1c.cloudfunctions.net/mumbai_token_transfer"
+#function_url = "http://127.0.0.1:5000/transfer"
+
+#with open('token.txt', 'r') as file:
+#    firebase_id_token = file.read().strip()
+
+firebase_id_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjYzODBlZjEyZjk1ZjkxNmNhZDdhNGNlMzg4ZDJjMmMzYzIzMDJmZGUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vd2FsbGV0LWxvZ2luLTQ1YzFjIiwiYXVkIjoid2FsbGV0LWxvZ2luLTQ1YzFjIiwiYXV0aF90aW1lIjoxNjkyNjA5NzY3LCJ1c2VyX2lkIjoiNEVmZ0tLOUZzVVZMMzVvTktXU1EyMVZHNXB6MiIsInN1YiI6IjRFZmdLSzlGc1VWTDM1b05LV1NRMjFWRzVwejIiLCJpYXQiOjE2OTI2MDk3NjgsImV4cCI6MTY5MjYxMzM2OCwiZW1haWwiOiJib2JAZXhhbXBsZS5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsiYm9iQGV4YW1wbGUuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.HL0XQgtKAdqcewr70NqRs_fKQNxCiyEV0R4glxJjRQdVg_Es6gomsNErbLwUZ5tAg_HROzHO2dWq7rbh15eOOAYuhhjw7PFKhyuGp8Aku9Hkvo1-iazp7vtC3gLCO4lyIBdPsWhbNnhBIwx99GLaLth-h7f1n4jgSoBDmn6EN0ixoBReqncYpWdZnED2cBqDU_ZLFZVV3mwDL6DtEHS0pbQD6ZMQzxzUvtJZuxKMPToFy8i4DKI-iReD7hSczWWv1-_79TPI62WIFZGlF22VkppbG5rW6kHvdMaIHo_nOPEb08QG7ByvEe2_n-wFaZjIf0cicGQ-UwI_Yr7jBUAj6A"
+
+
+
+# Replace with the transactions you want to send
+transactions = [
+    {
+        "to_address": "0xFA8b27A0d9C5Bf7490ea03eDAB652c3a18cAc19b",
+        "amount": "600"
+    }
+]
+
+# Set the headers for the request
+headers = {
+    'Authorization': f'Bearer {firebase_id_token}',
+    "Content-Type": "application/json"
+}
+
+
+for tx in transactions:
+    json_payload = {
+        "transactions": [tx]
+    }
+
+    headers = {
+        'Authorization': f'Bearer {firebase_id_token}',
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(function_url, json=json_payload, headers=headers)
+
+    print(response.status_code)
+    print(response.text)
+    #print("wait 3 sec")
+
+    #time.sleep(1)  # Wait for 1 second before sending the next request
---
+++ gcp_cloud_functions/mumbai_token_transfer/test_api3.py
@@ -0,0 +1,186 @@
+import json
+from web3 import Web3
+from decimal import Decimal, getcontext
+
+import firebase_admin
+from firebase_admin import credentials, auth
+from google.cloud import storage
+
+#from flask import request, jsonify
+from flask import Flask, request, jsonify
+from main import token_transfer
+
+app = Flask(__name__)
+
+@app.route('/transfer', methods=['POST'])
+def transfer_endpoint():
+    result = token_transfer(request)
+    return result
+
+# Read the ABI data from GLCToken.json
+with open('GLCToken.json', 'r') as file:
+    contract_data = json.load(file)
+
+# Set the precision for decimal calculations (adjust this based on your requirements)
+getcontext().prec = 28
+
+# Set up the Polygon (Mumbai) network
+polygon_rpc_url = "https://rpc-mumbai.maticvigil.com"  # Replace with the appropriate RPC endpoint
+w3 = Web3(Web3.HTTPProvider(polygon_rpc_url))
+
+# Replace these variables with your actual values
+private_key = '0xef9d1a761a291eea5e37529ff89030bc745d87e01e94039efe56ee6e681bcd5b'
+wallet_address = '0x717320e0Ea465dD9ee7b0345219DB1A6b2884869'
+
+# Create a mock request object with headers and data
+class MockRequest:
+    def __init__(self, authorization, data):
+        self.headers = {'Authorization': authorization}
+        self.data = data
+
+
+def initialize_firebase_admin_sdk():
+
+    try: 
+        # Replace with your Cloud Storage bucket and the path to the service account credentials JSON file
+        bucket_name = "gladius-backend"
+        credentials_file_path = "firebase-admi-sdk-d55dc53a0acc.json"
+
+        # Download the service account credentials JSON file from Cloud Storage
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(credentials_file_path)
+        credentials_file_content = blob.download_as_text()
+
+        # Initialize the Firebase Admin SDK with the downloaded service account credentials
+        cred = credentials.Certificate(credentials_file_content)
+        firebase_admin.initialize_app(cred)
+
+        # If the above doesn't work:
+        # Create a temporary file and write the content of the credentials JSON to it
+        # with open("/tmp/serviceAccountKey.json", "w") as tmp_file:
+        #    tmp_file.write(credentials_file_content)
+
+        # Initialize the Firebase Admin SDK with the downloaded service account credentials
+        #cred = credentials.Certificate("/tmp/serviceAccountKey.json")
+        #firebase_admin.initialize_app(cred)
+
+    except Exception as e:
+            print("Error initializing Firebase Admin SDK:", e)
+
+def generate_custom_token(uid):
+    
+    custom_token = auth.create_custom_token(uid)
+    return custom_token
+
+
+# Function to transfer tokens
+def token_transfer(request):
+    # Get the Firebase ID token from the Authorization header
+    bearer_token = request.headers.get("Authorization")
+    if not bearer_token:
+        return jsonify({"error": "Authorization token not found"}), 401
+
+    id_token = bearer_token.split(" ")[1]       
+    
+
+
+    try:
+        # Verify the Firebase ID token
+        decoded_token = auth.verify_id_token(id_token)
+
+        # Access user information from the decoded token
+        uid = decoded_token["uid"]
+        email = decoded_token["email"]
+        # Add any other user-related data you need
+
+        #####################################
+        # Cloud Function logic start
+
+        request_json = json.loads(request)  # Deserialize the JSON string to a dictionary
+        if 'to_address' in request_json and 'amount' in request_json:
+            to_address = request_json['to_address']
+            amount = Decimal(request_json['amount'])  # Convert the amount to a Decimal
+
+            # GLC Token contract address
+            token_contract_address = "0x7A57269A63F37244c09742d765B18b1852078072"
+
+            # Replace with the token decimals (usually 18 for most ERC20 tokens)
+            token_decimals = 18
+
+            # Convert the amount to the token's base unit
+            amount_in_base_unit = int(amount * 10**token_decimals)
+
+            # Load the contract ABI (Replace this with the actual ABI of your token contract)
+            token_abi = contract_data['abi']  # Put your token contract ABI here
+
+            # Instantiate the contract
+            contract = w3.eth.contract(address=token_contract_address, abi=token_abi)
+
+            
+            # Check if the request is for a mint or transfer operation
+            if 'mint' in request_json and request_json['mint']:
+                # Mint new tokens
+                transaction = contract.functions.mint(to_address, amount_in_base_unit).buildTransaction({
+                    'chainId': 80001,  # Replace with the appropriate chain ID (Mumbai network has chain ID 80001)
+                    'gas': 200000,
+                    'gasPrice': Web3.toWei('10', 'gwei'),  # Use Web3.toWei function directly
+                    'nonce': w3.eth.getTransactionCount(w3.toChecksumAddress(wallet_address))
+                    #'value': 0,  # Set the value to 0 for ERC20 token transfers
+                })
+            else:
+                # Transfer tokens
+                transaction = contract.functions.transfer(to_address, amount_in_base_unit).buildTransaction({
+                    'chainId': 80001,  # Replace with the appropriate chain ID (Mumbai network has chain ID 80001)
+                    'gas': 200000,
+                    'gasPrice': Web3.toWei('10', 'gwei'),  # Use Web3.toWei function directly
+                    'nonce': w3.eth.getTransactionCount(w3.toChecksumAddress(wallet_address))
+                    #'value': 0,  # Set the value to 0 for ERC20 token transfers
+                })
+
+
+            # Sign the transaction
+            signed_transaction = w3.eth.account.signTransaction(transaction, private_key)
+
+            # Send the transaction
+            tx_hash = w3.eth.sendRawTransaction(signed_transaction.rawTransaction)
+
+        # Cloud Function logic end
+        # #####################################
+
+            return f'Transaction sent. Transaction hash: {tx_hash.hex()}'
+
+    except json.JSONDecodeError:
+        return 'Invalid JSON payload.'
+
+    except KeyError:
+        return 'Invalid request. Please provide "to_address" and "amount" in the JSON payload.'
+
+if __name__ == "__main__":
+
+    app.run()
+
+    # Call this function to initialize the Firebase Admin SDK in your Cloud Function
+    initialize_firebase_admin_sdk()
+
+    uid = '4EfgKK9FsUVL35oNKWSQ21VG5pz2' #Bob
+    custom_token = auth.create_custom_token(uid)
+    
+    authorization = "Bearer " + custom_token
+    print(authorization)
+
+    json_payload = {
+        "to_address": "0xce912F29932994e60A7aEEa9F18F7C16E086CBAc",
+        "amount": "0.01",
+        "mint": False  # Set to True if you want to perform a mint operation
+    }
+    data = json.dumps(json_payload)
+
+    # Create a mock request
+    mock_request = MockRequest(authorization, data)
+
+    # Call the token_transfer function with the mock request
+    result = token_transfer(mock_request)
+
+    # Print the result
+    print(result)
