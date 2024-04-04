1d88b3eb8cf81fdc7731f7b00118e541e5a44393
Alexey <as.lexus@gmail.com>
2024-04-04 10:55:21 +0300

    CF invokeGladiusTransaction backup

---
+++ gcp_cloud_functions/invokeGladiusTransaction/test_cf.sh
@@ -0,0 +1,3 @@
+curl -X POST "https://europe-west1-wallet-login-45c1c.cloudfunctions.net/invokeGladiusTransaction" \
+-H "Content-Type:application/json" \
+-d '{"UID":"40WiH4RtOIgtJxGjwO6vadjAOem2", "amount":"0"}'
