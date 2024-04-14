947ad6ca386e4644a270049579b66d0bbdab1762
Alexey Sirotkin <alexey.sirotkin@telia.ee>
2024-04-14 22:10:50 +0300

    Update test_cf.sh

--- gcp_cloud_functions/SignupGladiusParent/test_cf.sh
+++ gcp_cloud_functions/SignupGladiusParent/test_cf.sh
@@ -2,5 +2,7 @@ curl -X POST "https://europe-west1-wallet-login-45c1c.cloudfunctions.net/SignupG
 -H "Content-Type: application/json" \
 -d '{
   "StudentUID": "pUZViZsflQVT1JbLP6oM6n06tEk1",
-  "ParentUID": "3NCRR1ZPmgYRDxCOoIq8svoQtc92"
+  "ParentUID": "3NCRR1ZPmgYRDxCOoIq8svoQtc92",
+  "ClubUID":  "2",
+  "GroupUID" ""
 }'
