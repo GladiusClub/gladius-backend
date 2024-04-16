27e76b4c25051b3c7c578706dc50326407f3cb61
Alexey <as.lexus@gmail.com>
2024-04-16 22:33:33 +0300

    cleanup

--- stellar_functions/stellar_get_balance/test_getStudentBalance.sh
+++
@@ -1,6 +0,0 @@
-#curl -X POST "https://europe-west1-wallet-login-45c1c.cloudfunctions.net/getStudentBalance?publicKey=GA2ZSBCXUKCOQLOMPLGAJ2JR2RBNC4UGFYKYIYO2VQEPJCNJ27I5E73L" 
-
-#curl -X POST "https://europe-west1-wallet-login-45c1c.cloudfunctions.net/getStudentBalance?publicKey=GDKT3YL6QCPPJZ53R7PUN6VX7F2SFZNSYCGALC7DIUVNHEV5IJSNKFRM" 
-
-curl -X POST "https://europe-west1-wallet-login-45c1c.cloudfunctions.net/getStudentBalance?publicKey=GA7XAJ7C6VFMJCTFQTVNQ5NE4VTZUZQZUOIP3UQXXOQS2F5RTYUHULF2" 
-
--- stellar_get_balance/dist/cloud_function_deploy.sh
+++
@@ -1,5 +0,0 @@
-gcloud functions deploy getStudentBalance --gen2 --runtime nodejs18 \
---trigger-http \
---entry-point=getStudentBalance \
---region=europe-west1 \
---allow-unauthenticated 
